3ca20f2feb443e9bcb71826ece1c5d207c56c147
shekharbtn <shekharbtn@gmail.com>
2022-01-25 18:14:31 +0530

    code cleaned

--- UserController.js
+++ UserController.js
@@ -69,12 +69,6 @@ function getUserByEmail({ email }) {
   }
 }
 
-// response = register({
-//   name: 'subu haldar',
-//   email: 'subu@gmail.com',
-//   password: 'Subuhld@123',
-// });
-
 response = getUserByEmail({ email: "subu@gmail.com" });
 
 // login function
@@ -112,50 +106,7 @@ response = login({
   password: "Subuhld@123",
 });
 
-// console.log(response);
-
-// get users by limit and offset
-// const GetData = ({limit,offset})=>{
-//   try{
-//     const userDatalen = userData.length
-
-//     if(us)
-//   }
-// }
-
-const Users = require("./users.json");
-
-// users = 100 records
-
-// records = 1 to 10
-
-// limit = 10
-// offset = 3
-
-// records = 21 to 30
-
-// limit = 6
-// offset = 1
-// records = 1 to 6
-
-// limit = 6
-// offset = 2
-// records = 7 to 12
-
-// limit = 6
-// offset = 5
-// records = 25 to 30
-
-// limit = 6
-// offset = 3
-// records = 13 to 18
-
-// const pagination = (limit, offset) => {
-//   const dataLimit = limit * offset;
-//   const findUser = Users.slice(dataLimit, limit);
-//   console.log(findUser);
-// };
-
+//Pagination Function
 function pagination(limit = 10, offset = 1) {
   try {
     if (!Users.length) {
