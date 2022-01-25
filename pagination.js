b15dc2a829f6d8562a213ec23655e0d39a0a5fc9
shubhankar <shubhankarroy974@gmail.com>
2022-01-25 16:37:10 +0530

    pagination added

---
+++ pagination.js
@@ -0,0 +1,24 @@
+// users = 100 records
+
+// limit = 10
+// offset = 1
+
+// records = 1 to 10
+
+// limit = 10
+// offset = 3
+
+// records = 21 to 30
+
+// limit = 6
+// offset = 1
+// records = 1 to 6
+
+// limit = 6
+// offset = 2
+// records = 7 to 12
+
+// limit = 6
+// offset = 5
+
+// records = 25 to 30
