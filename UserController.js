558ae330dd5868615ac28a05e7e5c136c38267b8
rajnish <kumarrajnish523@gmail.com>
2022-01-25 13:42:01 +0530

    Usercontroller register func added

---
+++ UserController.js
@@ -0,0 +1,38 @@
+const users = [];
+let response = {};
+
+const register = ({ name, email, password }) => {
+  try {
+    // Name Validation
+    if (name.length < 5) {
+      throw { message: 'Name must be more than 5 char', status: 400 };
+    }
+
+    // Email Validation
+
+    // Password Validation
+
+    users.push({ name, email, password });
+    return {
+      message: 'User added successfully',
+      status: 200,
+      error: false,
+      data: users,
+    };
+  } catch (error) {
+    return { message: error.message, status: error.status, error: true };
+  }
+};
+
+response = register({
+  name: 'varu',
+  email: 'varun@gmail.com',
+  password: 'Varun@123',
+});
+
+// Name must be more than 5 char
+// Email must be valid
+// Password must contains 1 numeric, 1Uppercase, and more than 8 char
+
+// console.log(users);
+console.log('errorMessage', response);
