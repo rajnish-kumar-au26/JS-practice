3dc772f0b8b73d7a636d03f747531db94095c8bd
shekharbtn <shekharbtn@gmail.com>
2022-01-25 18:03:02 +0530

    Pagination function added

--- pagination.js
+++ pagination.js
@@ -1,7 +1,6 @@
-// users = 100 records
+const Users = require("./users.json");
 
-// limit = 10
-// offset = 1
+// users = 100 records
 
 // records = 1 to 10
 
@@ -20,5 +19,38 @@
 
 // limit = 6
 // offset = 5
-
 // records = 25 to 30
+
+// limit = 6
+// offset = 3
+// records = 13 to 18
+
+// const pagination = (limit, offset) => {
+//   const dataLimit = limit * offset;
+//   const findUser = Users.slice(dataLimit, limit);
+//   console.log(findUser);
+// };
+
+function pagination(limit = 10, offset = 1) {
+  try {
+    if (!Users.length) {
+      throw { message: "No Data", error: true, status: 400 };
+    }
+    const dataLimit = limit * offset;
+    const initialData = dataLimit - limit;
+    let userData = Users.slice(initialData, dataLimit);
+    return {
+      message: "Success",
+      status: 200,
+      count: Users.length,
+      error: false,
+      data: userData,
+    };
+  } catch (error) {
+    return { message: error.message, status: error.status, error: error.error };
+  }
+}
+const limit = 5;
+const offset = 2;
+// pagination(limit, offset);
+console.log(pagination());
