f3b3c9178717a6aed93797086a98f15515cdf086
rajnish <kumarrajnish523@gmail.com>
2022-01-26 13:03:15 +0530

    bcrypt password

--- UserController.js
+++ UserController.js
@@ -1,26 +1,27 @@
+const bcrypt = require('bcryptjs');
 const users = [
   {
-    name: "subu haldar",
-    email: "subu@gmail.com",
-    password: "Subuhld@123",
+    name: 'subu haldar',
+    email: 'subu@gmail.com',
+    password: 'Subuhld@123',
   },
 ];
 
-const userData = require("./users.json");
-let response = {};
+// const userData = require('./users.json');
+// let response = {};
 
-const register = ({ name, email, password }) => {
+const register = async ({ name, email, password }) => {
   try {
     //   Name Validation
     if (name.length < 5) {
-      throw { message: "Name must be more than 5 char", status: 400 };
+      throw { message: 'Name must be more than 5 char', status: 400 };
     }
 
     // Email Validation
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!email.match(mailformat)) {
       throw {
-        message: "You have entered an invalid email address!",
+        message: 'You have entered an invalid email address!',
         status: 400,
       };
     }
@@ -28,17 +29,19 @@ const register = ({ name, email, password }) => {
     // Password Validation
     const passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
     if (!password.match(passw)) {
-      throw { message: "Password validation error", status: 400 };
+      throw { message: 'Password validation error', status: 400 };
     }
 
     let findUser = users.filter((user) => user.email === email);
     if (findUser.length) {
-      throw { message: "Email already exist", status: 400 };
+      throw { message: 'Email already exist', status: 400 };
     }
-
-    users.push({ name, email, password });
+    const tt = await bcrypt.hash(password, 10);
+    console.log(tt);
+    users.push({ name, email, password: tt });
+    // console.log(users);
     return {
-      message: "User added successfully",
+      message: 'User added successfully',
       status: 200,
       error: false,
       // data: users,
@@ -48,14 +51,22 @@ const register = ({ name, email, password }) => {
   }
 };
 
+register({
+  name: 'Shekhar haldar',
+  email: 'shekhar@gmail.com',
+  password: 'Shekhar@123',
+});
+
+console.log(users);
+
 function getUserByEmail({ email }) {
   try {
     let getUser = users.filter((user) => user.email === email);
     if (!getUser.length) {
-      throw { status: 400, message: "user is not found" };
+      throw { status: 400, message: 'user is not found' };
     }
     return {
-      message: "User found successfully",
+      message: 'User found successfully',
       status: 200,
       error: false,
       data: getUser[0],
@@ -69,7 +80,7 @@ function getUserByEmail({ email }) {
   }
 }
 
-response = getUserByEmail({ email: "subu@gmail.com" });
+response = getUserByEmail({ email: 'subu@gmail.com' });
 
 // login function
 const login = ({ email, password }) => {
@@ -77,17 +88,17 @@ const login = ({ email, password }) => {
     // email validation
     const isEmail = users.filter((user) => user.email === email);
     if (!isEmail.length) {
-      throw { message: "email id not register", status: "400" };
+      throw { message: 'email id not register', status: '400' };
     }
 
     // password validation
     if (isEmail[0].password != password) {
-      throw { message: "incorrect password", status: "400" };
+      throw { message: 'incorrect password', status: '400' };
     }
 
-    const token = "jyjtcux646rx76cry";
+    const token = 'jyjtcux646rx76cry';
     return {
-      message: "User successfully logged in",
+      message: 'User successfully logged in',
       status: 200,
       error: false,
       data: { token: token },
@@ -102,21 +113,20 @@ const login = ({ email, password }) => {
 };
 
 response = login({
-  email: "subu@gmail.com",
-  password: "Subuhld@123",
+  email: 'subu@gmail.com',
+  password: 'Subuhld@123',
 });
 
-//Pagination Function
 function pagination(limit = 10, offset = 1) {
   try {
     if (!Users.length) {
-      throw { message: "No Data", error: true, status: 400 };
+      throw { message: 'No Data', error: true, status: 400 };
     }
     const dataLimit = limit * offset;
     const initialData = dataLimit - limit;
     let userData = Users.slice(initialData, dataLimit);
     return {
-      message: "Success",
+      message: 'Success',
       status: 200,
       count: Users.length,
       error: false,
@@ -128,5 +138,15 @@ function pagination(limit = 10, offset = 1) {
 }
 const limit = 5;
 const offset = 2;
-// pagination(limit, offset);
+pagination(limit, offset);
 console.log(pagination());
+
+let getHashPassword = async function () {
+  try {
+    users.map(async (user) => {
+      user.password = await bcrypt.hash(user.password, 10);
+      console.log(user);
+    });
+  } catch (error) {}
+};
+// getHashPassword();
