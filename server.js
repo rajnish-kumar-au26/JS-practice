f81c1620323964da7294a6c31d30d5b3fdd994dc
rajnish <kumarrajnish523@gmail.com>
2022-01-26 16:54:51 +0530

    ProductService all Class methods Done

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
-const userService = require("./services/userService");
-const productsService = require("./services/productService");
-const blogService = require("./services/blogService");
+const userService = require('./services/userService');
+const productsService = require('./services/productService');
+const blogService = require('./services/blogService');
 
-let response = userService.getAllUsers(10, 1);
+// let response = userService.getAllUsers(10, 1);
+// console.log(response);
 
-console.log(response);
+// // for test addProduct Function
+// let responseProduct = productsService.addProduct({
+//   name: 'tata',
+//   description: 'tata watch',
+//   price: '5000',
+//   image: 'tataimagelink',
+// });
+// console.log(responseProduct);
+
+// // for test update function
+// let product = {
+//   name: 'sabu',
+//   description: 'subu titan watch',
+//   price: '5000',
+//   image: 'imagelinkupdated',
+// };
+// let id = '5d80a554-6de7-4d0e-ac93-1dbb2f18807a';
+// let responseProductUpdate = productsService.updateProduct(id, product);
+// console.log(responseProductUpdate);
+
+// //for test deleteProduct function
+// let id = 'c6368326-6a33-41ac-8491-e0d29b11f1d1';
+// let responseDeleteProduct = productsService.deleteProduct(id);
+// console.log(responseDeleteProduct);
+
+// // get ProductbyID test function
+// let id = '09f07ec8-dccb-4411-899e-d91951424ae3';
+// let data = productsService.getProduct(id);
+// console.log(data);
+
+// // get allProducts function test
+
+let getAllProducts = productsService.getAllProduct();
+console.log(getAllProducts);
--- services/productService.js
+++ services/productService.js
@@ -1,5 +1,198 @@
-const products = require("../db/products.json");
+const products = require('../db/products.json');
+const { v4 } = require('uuid');
+const fs = require('fs');
+const path = require('path');
+let reqPath = path.join(__dirname, '../db/products.json');
 
-class ProductsService {}
+class ProductsService {
+  addProduct = async ({ name, description, price, image }) => {
+    try {
+      //   Name Validation
+      if (!name.length) {
+        throw { message: 'Name must be required', status: 400, error: true };
+      }
+
+      //   Description Validation
+      if (!description.length) {
+        throw {
+          message: 'Description must be required',
+          status: 400,
+          error: true,
+        };
+      }
+
+      //   Price Validation
+      if (!price.length) {
+        throw { message: 'Price must be required', status: 400, error: true };
+      }
+
+      //Image Validation
+      if (!image.length) {
+        throw { message: 'Image must be required', status: 400, error: true };
+      }
+
+      let findProductTitle = products.filter((product) => product.name == name);
+      //   console.log(findProductTitle);
+      if (findProductTitle.length) {
+        throw {
+          message: 'Title name is already exists',
+          status: 400,
+          error: true,
+        };
+      }
+
+      let productDetails = {
+        id: v4(),
+        name,
+        description,
+        price,
+        image,
+        date: new Date(),
+      };
+
+      products.push(productDetails);
+      fs.writeFile(reqPath, JSON.stringify(products), (error) => {
+        if (error) {
+          throw error;
+        }
+      });
+      //   console.log(productDetails);
+      return {
+        message: 'Product added successfully',
+        status: 200,
+        error: false,
+        // data: productDetails,
+      };
+    } catch (error) {
+      return { message: error.message, status: error.status, error: true };
+    }
+  };
+
+  updateProduct = async (productID, newProduct) => {
+    try {
+      //   productID Validation
+      if (!productID.length) {
+        throw {
+          message: 'ProductID must be required',
+          status: 400,
+          error: true,
+        };
+      }
+      let findProductByID = products.filter(
+        (product) => product.id == productID
+      );
+      //   console.log(findProductByID);
+      if (!findProductByID.length) {
+        throw {
+          message: 'ProductID  is not Found',
+          status: 400,
+          error: true,
+        };
+      }
+      const { name, description, price, image } = newProduct;
+      //   console.log(name, description, price, image);
+      let productTitleName = findProductByID[0].name;
+      let productDescription = findProductByID[0].description;
+      let productPrice = findProductByID[0].price;
+      let productImage = findProductByID[0].image;
+      //   let ceratedDate = findProductByID[0].date;
+      //   let modifiedDate = new Date();
+      if (name.length) {
+        productTitleName = name;
+      }
+      if (description.length) {
+        productDescription = description;
+      }
+      if (price.length) {
+        productPrice = price;
+      }
+      if (image.length) {
+        productImage = image;
+      }
+
+      let index = products.findIndex((user) => user.id == productId);
+      //   console.log(index);
+      products[index].name = productTitleName;
+      products[index].description = productDescription;
+      products[index].price = productPrice;
+      products[index].image = productImage;
+
+      fs.writeFile(reqPath, JSON.stringify(products), (error) => {
+        if (error) {
+          throw error;
+        }
+      });
+      return {
+        message: 'Product Updated successfully',
+        status: 200,
+        error: false,
+      };
+    } catch (error) {
+      return { message: error.message, status: error.status, error: true };
+    }
+  };
+
+  deleteProduct = async (productID) => {
+    try {
+      let index = products.findIndex((user) => user.id == productID);
+      if (index == -1) {
+        throw { message: 'Id is Not Found', status: 400, error: true };
+      }
+      products.splice(index, 1);
+      fs.writeFile(reqPath, JSON.stringify(products), (error) => {
+        if (error) {
+          throw error;
+        }
+      });
+      return {
+        message: 'Product Deleted successfully',
+        status: 200,
+        error: false,
+      };
+    } catch (error) {
+      return { message: error.message, status: error.status, error: true };
+    }
+  };
+
+  getProduct = async (productID) => {
+    try {
+      let getData = products.filter((product) => product.id === productID);
+      if (!getData.length) {
+        throw { message: 'Id is Not Found', status: 400, error: true };
+      }
+      return {
+        message: 'Product GET successfully',
+        status: 200,
+        error: false,
+        data: getData[0],
+      };
+    } catch (error) {
+      return { message: error.message, status: error.status, error: true };
+    }
+  };
+
+  getAllProduct = async (offset = 1, limit = 10) => {
+    try {
+      if (!products.length) {
+        throw { message: 'data not found', status: 200 };
+      }
+      let end = limit * offset;
+      let start = end - limit;
+      let data = products.slice(start, end);
+      return {
+        message: 'get user succesfully',
+        status: 200,
+        error: false,
+        data: { count: products.length, rows: data },
+      };
+    } catch (error) {
+      return {
+        message: error.message,
+        status: error.status,
+        error: true,
+      };
+    }
+  };
+}
 
 module.exports = new ProductsService();
