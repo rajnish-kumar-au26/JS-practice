dfe1fda2a2c5906f4e8e72c9062d4499e576594a
shekharbtn <shekharbtn@gmail.com>
2022-01-27 13:38:45 +0530

    changes

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const userService = require('./services/userService');
-const productsService = require('./services/productService');
-const blogService = require('./services/blogService');
+const userService = require("./services/userService");
+const productsService = require("./services/productService");
+const blogService = require("./services/blogService");
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const userRouter = require('./api/userController/router');
+const userRouter = require("./api/userController/router");
 
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
-app.listen(4000, () => console.log('Server is running on port 4000'));
+app.listen(4000, () => console.log("Server is running on port 4000"));
