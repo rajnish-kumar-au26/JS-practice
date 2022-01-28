d8d8d174145e2b1f1234d3a8e8d8fe7d522776f9
shubhankar <shubhankarroy974@gmail.com>
2022-01-28 13:02:18 +0530

    modified blog router

--- api/blogController/router.js
+++ api/blogController/router.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const blogController = require('./controller');
-const jwt = require('../../middleware/jwtValidation');
+const router = require("express").Router();
+const blogController = require("./controller");
+const jwt = require("../../middleware/jwtValidation");
 
-router.post('/create', jwt.verifyToken, blogController.addBlog);
-router.put('/update', jwt.verifyToken, blogController.updateBlog);
-router.delete('/delete', jwt.verifyToken, blogController.deleteBlog);
-router.get('/', jwt.verifyToken, blogController.getBlogById);
-router.get('/list/:limit/:offset', jwt.verifyToken, blogController.getAllBlog);
+router.post("/create", jwt.verifyToken, blogController.addBlog);
+router.put("/update", jwt.verifyToken, blogController.updateBlog);
+router.delete("/delete", jwt.verifyToken, blogController.deleteBlog);
+router.get("/", jwt.verifyToken, blogController.getBlogById);
+router.get("/list/:limit/:offset", jwt.verifyToken, blogController.getAllBlog);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const userRouter = require('./api/userController/router');
-const productRouter = require('./api/productController/router');
-const blogRouter = require('./api/blogController/router');
+const userRouter = require("./api/userController/router");
+const productRouter = require("./api/productController/router");
+const blogRouter = require("./api/blogController/router");
 
-app.use('/user', userRouter);
-app.use('/product', productRouter);
-app.use('/blog', blogRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/blog", blogRouter);
 
-app.listen(4000, () => console.log('Server is running on port 4000'));
+app.listen(4000, () => console.log("Server is running on port 4000"));
