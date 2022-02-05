fb06e0230adb70c03ec6b1803978f34124229a99
rajnish <kumarrajnish523@gmail.com>
2022-02-05 19:53:49 +0530

    product and navbar component added

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,24 +1,35 @@
-import Register from "./components/register";
-import Login from "./components/login";
-import Home from "./components/home";
+import Register from './components/register';
+import Login from './components/login';
+import Home from './components/home';
+import { Product } from './components/product';
+import { Navbar } from './components/Navbar';
 // import Counter from "./components/counter";
 
-import { useState } from "react";
+import { useState } from 'react';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [token, setToken] = useState(false);
 
   return (
     <Router>
+      <Navbar loggedIn={loggedIn} />
       <Routes>
         <Route
           path="/login"
-          element={<Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
+          element={
+            <Login
+              loggedIn={loggedIn}
+              setLoggedIn={setLoggedIn}
+              setToken={setToken}
+            />
+          }
         />
         <Route path="/register" element={<Register loggedIn={loggedIn} />} />
-        <Route path="/" element={<Home loggedIn={loggedIn} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product token={token} />} />
       </Routes>
     </Router>
   );
--- client/myapp/src/components/login.js
+++ client/myapp/src/components/login.js
@@ -1,40 +1,35 @@
-import { useState } from "react";
-import axios from "axios";
-import { Navigate } from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
-function Login({ loggedIn, setLoggedIn }) {
+function Login({ loggedIn, setLoggedIn, setToken }) {
   const [user, setUser] = useState({});
-  const [message, setMessage] = useState("");
-  const [token, setToken] = useState("");
+  const [message, setMessage] = useState('');
+  // const [token, setToken] = useState("");
 
   const loginHandler = async () => {
-    const apiurl = "http://localhost:4000/user/login";
+    const apiurl = 'http://localhost:4000/user/login';
 
     const apiRes = await axios
-      .post(apiurl, user, {
-        headers: {
-          Authorization: token,
-        },
-      })
+      .post(apiurl, user)
       .then((res) => res)
       .catch((error) => error.response);
 
-    setMessage(apiRes.data.message);
-
     if (!apiRes.data.error) {
       const gotToken = apiRes.data.data.token;
+      setMessage(apiRes.data.message);
       setLoggedIn(true);
       setToken(gotToken);
 
-      const product = await axios
-        .get("http://localhost:4000/product/transaction/list", {
-          headers: {
-            Authorization: gotToken,
-          },
-        })
-        .then((res) => res)
-        .catch((error) => error.response);
-      console.log("API res", product.data);
+      // const product = await axios
+      //   .get('http://localhost:4000/product/transaction/list', {
+      //     headers: {
+      //       Authorization: gotToken,
+      //     },
+      //   })
+      //   .then((res) => res)
+      //   .catch((error) => error.response);
+      // console.log('API res', product.data);
     }
   };
 
@@ -65,7 +60,6 @@ function Login({ loggedIn, setLoggedIn }) {
     <div>
       <h2>Login Page</h2>
       <h4>{message}</h4>
-      <p>{token}</p>
       <input
         onChange={(e) => inputHandler(e)}
         name="email"
